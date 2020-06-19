106d8c8fd53143fd67cd8a5649e12ac9aa6c6ae7
Simon Burton <simon@arrowtheory.com>
2020-06-19 20:24:59 +0100

    commit edits

--- Raw HTML source/proc.py
+++ Raw HTML source/proc.py
@@ -71,6 +71,8 @@ assert do_tag(line, "sub", lambda s:"_{%s}"%s)=="called E_{8}!  E_{8} is known"
 
 ENTITIES = [
     ("&phi;", r'\phi '), # lowercase greek
+    ("&alpha;", r'\alpha '),
+    ("&beta;", r'\beta '),
     ("&pi;", r'\pi '),
     ("&rho;", r'\rho '),
     ("&lambda;", r'\lambda '),
@@ -84,7 +86,11 @@ ENTITIES = [
     ("&psi;", r'\psi '),
     ("&gamma;", r'\gamma '),
     ("&sigma;", r'\sigma '),
+    ("&nu;", r'\nu '),
+    ("&zeta;", r'\zeta '),
     ("&Gamma;", r'\Gamma '), # upercase greek
+    ("&Alpha;", r'\Alpha '),
+    ("&Beta;", r'\Beta '),
     ("&Pi;", r'\Pi '),
     ("&Rho;", r'\Rho '),
     ("&Lambda;", r'\Lambda '),
@@ -99,12 +105,18 @@ ENTITIES = [
     ("&Psi;", r'\Psi '),
     ("&Sigma;", r'\Sigma '),
     ("&rarr;", r'\to '), # symbols
+    ("&cong;", r'\cong '), # symbols
+    ("&cup;", r'\cup '), # symbols
+    ("&cap;", r'\cap '), # symbols
+    ("&sum;", r'\sum'),
+    ("&prime;", r"'"),
     ("&infin;", r'\infty '),
     ("&frac12;", r'\frac{1}{2} '),
     ("&otimes;", r'\otimes '),
     ("&times;", r'\times '),
     ("&oplus;", r'\oplus '),
     ("&#8224;", r'\dagger '),
+    ("&dagger;", r'\dagger '),
     ("&#295;", r'\hbar '),
     ("&lt;", '<'),
     ("&gt;", '>'),
@@ -113,6 +125,7 @@ ENTITIES = [
     ("&radic;", r'\sqrt '),
     ("&quot;", '"'),
     ("&amp;", r'\text{\&} '),
+    ("&nbsp;", r'\ '), # space
 ]
 REWRITES = [
     ("<P>", "\n"),
@@ -140,7 +153,9 @@ def do_line(line, is_math=False):
     line = do_tag(line, "sup", lambda s:r"^{%s}"%s)
 
     #assert "&" not in line, repr(line)
-    if "&" in line:
+    if "{&}" in line:
+        pass
+    elif "&" in line:
         print("warning: html entity in %r"%line)
 
     return line
